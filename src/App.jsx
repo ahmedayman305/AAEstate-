0c2b8491eadde346b225183c304bb0112e86d187
ahmedayman305 <ahmedaymankamel78@gmail.com>
2024-06-27 22:45:25 +0300

    add routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home, SignIn, SignUp, Profile, About } from "./pages/index";
 
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/sign-in", element: <SignIn /> },
+  { path: "/sign-out", element: <SignUp /> },
+  { path: "/profile", element: <Profile /> },
+  { path: "/about", element: <About /> },
+]);
 
 function App() {
-  return <div className="text-red-500">App</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as About } from "./About.jsx";
+export { default as SignIn } from "./SignIn.jsx";
+export { default as Home } from "./Home.jsx";
+export { default as SignUp } from "./SignUp.jsx";
+export { default as Profile } from "./Profile.jsx";
