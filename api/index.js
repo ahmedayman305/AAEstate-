76c6651c7d651a4e4759b614d2574b4567cd69b3
ahmedayman305 <ahmedaymankamel78@gmail.com>
2024-06-28 04:14:21 +0300

    add controllers and routes

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
-import express from 'express'
+import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import userRouter from './routes/user.route.js'; // Corrected path
 
 dotenv.config();
 connectDB();
 
-const app = express()
+const app = express();
+
+app.use('/api/users', userRouter); // Mount userRouter under '/users' path
 
 app.listen(3000, () => {
-    console.log(`server run on port 3000`)
-})
+    console.log(`Server running on port 3000`);
+});
